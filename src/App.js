0081ac6ec1645680681d76220683549c7646e70f
ujjwalbhandari14 <ujjwalbhandari14@gmail.com>
2022-09-07 18:00:46 +0545

    Home page fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import { React, useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -10,13 +9,10 @@ import Contact from "./components/contact us/Contact";
 import Footer from "./components/footer/footer";
 import Ourwork from "./components/ourwork/ourwork";
 import "aos/dist/aos.css";
-// import Aos from "aos";
 
 function App() {
   const [loader, setloader] = useState(true);
-  // useEffect(() => {
-  //   Aos.init({ easing: "ease", duration: 700 });
-  // });
+
   useEffect(() => {
     setTimeout(() => {
       setloader(false);
@@ -28,26 +24,20 @@ function App() {
       {loader ? (
         <Loader />
       ) : (
+        // <div className="Home-section">
         <Router>
-          <div className="Hero-section">
-            <div className="Home-section">
-              <div className="Nav-section">
-                <Navbar />
-              </div>
+          <Navbar />
 
-              <Routes>
-                <Route exact path="/" element={<Home />}></Route>
-                <Route exact path="/about" element={<About />}></Route>
-                <Route exact path="/contact" element={<Contact />}></Route>
-                <Route exact path="/work" element={<Ourwork />}></Route>
-              </Routes>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/contact" element={<Contact />}></Route>
+            <Route exact path="/work" element={<Ourwork />}></Route>
+          </Routes>
 
-              <div className="footer">
-                <Footer/>
-              </div>
-            </div>
-          </div>
+          <Footer />
         </Router>
+        // </div>
       )}
     </>
   );
