4b755c71d7fed2b5aeeac1bc55d3e29130fd13af
Anish <acharyaanish920@gmail.com>
2022-09-09 19:33:25 +0545

    nav bar scrolltop fixed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React} from "react";
 import logo from "../../Assets/logo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
@@ -11,6 +11,12 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+
+  const toFunction = ()=>{
+    document.body.scrollTop = 0; // for safari
+    document.documentElement.scrollTop = 0; // for chrome,firefox, IE and Opera
+  }
+
   return (
     <>
       <div className="main-div">
@@ -21,33 +27,33 @@ export default function Navbar() {
 
           <ul className="nav-ul">
             <li>
-              <Link className="list" to="/">
-                <div className="nav-text-icon">
+              <Link className={`list`} to="/"  onClick={toFunction}>
+                <div className={``}>
 
-                <FontAwesomeIcon icon={faHouse} />
+                <FontAwesomeIcon icon={faHouse}/>
                 <h3> HOME </h3>
                 </div>
               </Link>
             </li>
             <li>
-              <Link className="list" to="/work">
-              <div className="nav-text-icon">
+              <Link className="list" to="/work"  onClick={toFunction}>
+              <div className={``}>
                 <FontAwesomeIcon icon={faBookOpen} />
                 <h3> OUR WORK </h3>
                 </div>
               </Link>
             </li>
             <li>
-              <Link className="list" to="/about">
-              <div className="nav-text-icon">
+              <Link className="list" to="/about"  onClick={toFunction}>
+              <div className={``}>
                 <FontAwesomeIcon icon={faBriefcase} />
                 <h3>ABOUT US </h3>
                 </div>
               </Link>
             </li>
             <li>
-              <Link className="list" to="/contact">
-              <div className="nav-text-icon">
+              <Link className="list" to="/contact"  onClick={toFunction}>
+              <div className={``}>
                 <FontAwesomeIcon icon={faPhone} />
                 <h3>CONTACT US </h3>
                 </div>
